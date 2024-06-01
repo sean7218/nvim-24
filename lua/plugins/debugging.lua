7132702bd815faad74b49b1531a4456232348d0f
Sean Zhang <sean7218@gmail.com>
2024-05-31 21:54:59 -0400

    backup

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -1,64 +1,53 @@
 return {
-	{
-		"nvim-neotest/nvim-nio",
-	},
-	{
-		"rcarriga/nvim-dap-ui",
-	},
-	{
-		"julianolf/nvim-dap-lldb",
-		dependencies = { "mfussenegger/nvim-dap" },
-		opts = {},
-	},
-	{
-		"mfussenegger/nvim-dap",
-		config = function()
-			local dap = require("dap")
-			local dapui = require("dapui")
+  {
+    "nvim-neotest/nvim-nio",
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+  },
+  {
+    "mfussenegger/nvim-dap",
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
 
-			require("dap-lldb").setup({
-        configurations = {
-          swift = {
-            {
-              name = "Debug SwiftFitTests",
-              type = "lldb",
-              request = "launch",
-              program = "/Applications/Xcode.app/Contents/Developer/usr/bin/xctest",
-              cwd = "${workspaceFolder}",
-              stopOnEntry = false,
-              args = { ".build/debug/swift-fitPackageTests.xctest" },
-            }
-          }
-        }
-      })
       require("dapui").setup({})
 
-			dap.configurations.swift = {
-        {
-					name = "Debug SwiftFitTests",
-					type = "lldb",
-					request = "launch",
-					program = "/Applications/Xcode.app/Contents/Developer/usr/bin/xctest",
-					cwd = "${workspaceFolder}",
-					stopOnEntry = false,
-					args = { ".build/debug/swift-fitPackageTests.xctest" },
+      dap.adapters.codelldb = {
+        type = "server",
+        port = "3232",
+        executable = {
+          command = '/Users/sean7218/extension/adapter/codelldb',
+          args = {"--port", "3232"},
         }
       }
 
-			dap.listeners.before.attach.dapui_config = function()
-				dapui.open()
-			end
-			dap.listeners.before.launch.dapui_config = function()
-				dapui.open()
-			end
-			dap.listeners.before.event_terminated.dapui_config = function()
-				dapui.close()
-			end
-			dap.listeners.before.event_exited.dapui_config = function()
-				dapui.close()
-			end
-			vim.keymap.set("n", "<Leader>dt", dap.toggle_breakpoint, {})
-			vim.keymap.set("n", "<Leader>dc", dap.continue, {})
-		end,
-	},
+      dap.configurations.swift = {
+        {
+          name = "Debug SwiftFitTests",
+          type = "codelldb",
+          request = "launch",
+          program = "/Applications/Xcode.app/Contents/Developer/usr/bin/xctest",
+          cwd = "${workspaceFolder}",
+          stopOnEntry = false,
+          args = { ".build/debug/swift-fitPackageTests.xctest" },
+        },
+      }
+
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
+      vim.keymap.set("n", "<Leader>dt", dap.toggle_breakpoint, {})
+      vim.keymap.set("n", "<Leader>dc", dap.continue, {})
+    end,
+  },
 }
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "tsserver" },
+        ensure_installed = { "lua_ls", "tsserver", "rust_analyzer" },
       })
     end,
   },
@@ -18,6 +18,11 @@ return {
     config = function()
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
       local lspconfig = require("lspconfig")
+      -- rust
+      lspconfig.rust_analyzer.setup{
+        capabilities = capabilities
+      }
+
       -- lua_la
       lspconfig.lua_ls.setup({
         capabilities = capabilities,
